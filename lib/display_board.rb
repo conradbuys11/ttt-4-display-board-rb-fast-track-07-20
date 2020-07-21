bb3cf44161e87c0771e0ca02da969a9c11fba8fe
Conrad Buys <conradbuys11@gmail.com>
2020-07-21 17:36:25 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-board = [" ", ]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board_arr)
+  puts " #{board[0]}  |  #{board[1]}  "
+end
