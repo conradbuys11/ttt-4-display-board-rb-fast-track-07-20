abf27eb388a5f937ce13c06459064e63c670862c
Conrad Buys <conradbuys11@gmail.com>
2020-07-21 17:37:50 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,5 +5,8 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board_arr)
   puts " #{board_arr[0]}  |  #{board_arr[1]} | #{board_arr[2]} "
-  puts
+  puts "-----------"
+  puts " #{board_arr[3]}  |  #{board_arr[4]} | #{board_arr[5]} "
+  puts "-----------"
+  puts " #{board_arr[6]}  |  #{board_arr[7]} | #{board_arr[8]} "
 end
